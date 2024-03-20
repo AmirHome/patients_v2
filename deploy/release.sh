8abbb8a4f53fba1c0c7b4db48ac60f14a9375f94
AmirHome <amir.email@yahoo.com>
2024-03-20 19:41:17 +0300

    Test Release bash script

--- deploy/release.sh
+++ deploy/release.sh
@@ -15,10 +15,9 @@ function extract_archive() {
   # Extract the archive to the root directory
   unzip -q "$download_folder/$archive_file" -d .
 
+    # (Optional) Remove the archive file after extraction
+    rm -f "$download_folder/$archive_file"
   echo "Extracted '$archive_file' to root directory."
-
-  cp deploy/.env .env
-  cp deploy/README.md README.md
 }
 
 # Function to clean root directory (excluding .git and deploy)
@@ -27,9 +26,22 @@ function clean_root() {
   echo "Cleaned root directory (excluding .git and deploy folder)."
 }
 
+# Function to develop laravel
+function deployment() {
+  cp deploy/.env .env
+  cp deploy/README.md README.md
+  composer update
+  php artisan migrate:fresh --seed
+  php artisan key:generate
+  php artisan storage:link
+
+  echo "Laravel development completed."
+}
+
 # Main script execution
 clean_root
 extract_archive
+deployment
 
 # Get the commit message argument (if provided)
 commit_message="$1"
@@ -44,5 +56,3 @@ else
   git push
 fi
 
-# (Optional) Remove the archive file after extraction
-# rm -f "$download_folder/$archive_file"
