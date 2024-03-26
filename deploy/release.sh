39f9f77913903f618b98412374e1cad23c2ba5da
AmirHome <amir.email@yahoo.com>
2024-03-26 10:29:21 +0300

    release

--- deploy/release.sh
+++ deploy/release.sh
@@ -4,26 +4,22 @@
 function clean_code() {
   git reset --hard
   git clean -df
-
   echo "Cleaned code git."
 }
 
 # Function to develop laravel
 function deployment() {
   # if .env file dose not exists, copy .env.local to .env
-  # cp deploy/.env.local .env
   if [ ! -f ".env" ]; then
     cp deploy/.env.local .env
   fi
   # if set argument update -u or --update, update composer
-  # composer update
   if [ "$1" == "-u" ] || [ "$1" == "--update" ]; then
     composer update
   fi
 
   # if set argument migrate -m or --migrate, migrate database
-  # php artisan migrate:fresh --seed
-  if [ "$1" == "-m" ] || [ "$1" == "--migrate" ]; then
+  if [ "$2" == "-m" ] || [ "$2" == "--migrate" ]; then
     php artisan migrate:fresh --seed
   fi
 
@@ -34,20 +30,24 @@ function deployment() {
 }
 
 # Main script execution
-clean_code
-extract_archive
-deployment
-
-# Get the commit message argument (if provided)
-commit_message="$1"
-
-# Check if a commit message was provided
-if [[ -z "$commit_message" ]]; then
-  echo "Skipping Git commands as no commit message provided."
+if [ $# -eq 0 ]; then
+  deployment
+elif [ $# -eq 1 ]; then
+  case $1 in
+    "clean")
+      clean_code
+      ;;
+    "-u" | "--update")
+      deployment "$1"
+      ;;
+    "-m" | "--migrate")
+      deployment "" "$1"
+      ;;
+    *)
+      echo "Invalid argument: $1"
+      exit 1
+      ;;
+  esac
 else
-  # Git add, commit and push with provided message
-  git add .
-  git commit -m "$commit_message"
-  git push
+  deployment "$1" "$2"
 fi
-
