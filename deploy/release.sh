224e9da336fc3a9593b79298eac6aff9e05b364c
AmirHome <amir.email@yahoo.com>
2024-03-27 13:20:11 +0300

    update release htaccess php8.3

--- deploy/release.sh
+++ deploy/release.sh
@@ -4,29 +4,33 @@
 function clean_code() {
   git reset --hard
   git clean -df
-  echo "Cleaned code git."
+  echo -e "\e[34mCleaned code from git.\e[0m"
 }
 
 
 function pull_code() {
   git pull origin master
+  # echo "Pulled code from master." by blue color
+  echo -e "\e[34mPulled code from master.\e[0m"
 }
 
 # Function to develop laravel
 function deployment() {
-  # where is now (pwd)
   echo "Current directory: $(pwd)"
 
   # if .env file dose not exists, copy .env.local to .env
   if [ ! -f ".env" ]; then
     cp deploy/.env.local .env
+    # echo "Copied .env.local to .env." by blue color
+    echo -e "\e[34mCopied .env.local to .env.\e[0m"
   fi
+
+  cp deploy/.htaccess public/.htaccess
+
   # if set argument update -u or --update, update composer
   # if [ "$1" == "-u" ] || [ "$1" == "--update" ]; then
     # composer update
   # fi
-
-
 
   /usr/local/bin/ea-php83 artisan key:generate
   /usr/local/bin/ea-php83 artisan storage:link
