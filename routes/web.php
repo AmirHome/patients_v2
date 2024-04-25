d75447c0869a9de140c4f2904a0c718d913f1285
AmirHome <amir.email@yahoo.com>
2024-04-25 18:54:37 +0300

    Fixed user data migration

--- routes/web.php
+++ routes/web.php
@@ -214,5 +214,5 @@ Route::group(['prefix' => 'profile', 'as' => 'profile.', 'namespace' => 'Auth', 
         Route::post('profile/destroy', 'ChangePasswordController@destroy')->name('password.destroyProfile');
     }
 });
-Route::get('/counter','\App\Livewire\Counter');
+Route::get('/travel','\App\Livewire\Counter');
 Route::get('/travel','\App\Livewire\Travel');
