45a65006648dbf5a8448a3862b9f0b451a0b453c
Ahmet İzgördü <ahmet.izgordu@econtech.com.tr>
2024-05-27 15:23:24 +0300

    Travel Start

--- resources/views/admin/travels/relationships/formFilter.blade.php
+++ resources/views/admin/travels/relationships/formFilter.blade.php
@@ -1,40 +1,32 @@
-<div class="card">
-
+<container>
+<div class="card-header-text">
+        <h3 class="text-left pb-3 font-weight-bold">Vakalar</h3>
+    </div>
+<div class="card card-table">
+<h4 class="form-title-text pt-4 pl-3 font-weight-bold">Vaka Listeleme</h4>
     <div class="card-body">
         <form action="{{ route('admin.travel-statuses.index') }}" method="get">
 
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-md-3">
                     <div class="form-group">
-                        <label for="">Patient Code</label>
-                        <input type="text" class="form-control filter" placeholder="Enter patient code"
+                        <input type="text" class="form-control form-text-input filter" placeholder="Hasta Kodu"
                             name="patient_code">
                         <span class="text-danger">@error('patient_code'){{ $message }}@enderror</span>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="form-group">
-                        <label for="">Patient name</label>
-                        <input type="text" class="form-control filter" placeholder="Enter patient name"
+                        <input type="text" class="form-control form-text-input filter" placeholder="Hasta Adı"
                             name="patient_name">
 
                         <span class="text-danger">@error('patient_name'){{ $message }}@enderror</span>
                     </div>
                 </div>
-                <div class="col-md-5">
+                <div class="col-md-3">
                     <div class="form-group">
-                        <label for="">Last name</label>
-                        <input type="text" class="form-control filter" placeholder="Enter last name" name="surname">
-                        <span class="text-danger">@error('surname'){{ $message }}@enderror</span>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-2">
-                    <div class="form-group">
-                        <label for="">Gender</label>
-                        <select class="form-control filter" wire:model.live="gender">
-                            <option value="" selected>Choose gender</option>
+                        <select class="form-control form-text-input filter" wire:model.live="gender">
+                            <option value="" selected>Cinsiyet</option>
                             @foreach ($genders as $key => $title)
                             <option value="{{ $key }}">{{ $title }}</option>
                             @endforeach
@@ -42,10 +34,20 @@
                         <span class="text-danger">@error('gender'){{ $message }}@enderror</span>
                     </div>
                 </div>
-                <div class="col-md-2">
+                <!-- <div class="col-md-5">
+                    <div class="form-group">
+                        <label for="">Last name</label>
+                        <input type="text" class="form-control form-text-input filter" placeholder="Enter last name" name="surname">
+                        <span class="text-danger">@error('surname'){{ $message }}@enderror</span>
+                    </div>
+                </div> -->
+            </div>
+            <div class="row">
+           
+                <!-- <div class="col-md-2">
                     <div class="form-group">
                         <label for="">Birthday</label>
-                        <input type="date" class="form-control filter" placeholder="Enter your birthday"
+                        <input type="date" class="form-control form-text-input filter" placeholder="Enter your birthday"
                             name="birthday">
                         <span class="text-danger">@error('birthday'){{ $message }}@enderror</span>
                     </div>
@@ -53,7 +55,7 @@
                 <div class="col-md-2">
                     <div class="form-group">
                         <label for="">Birth place</label>
-                        <input type="text" class="form-control filter" placeholder="Enter your birth place"
+                        <input type="text" class="form-control form-text-input filter" placeholder="Enter your birth place"
                             name="birth_place">
                         <span class="text-danger">@error('birth_place'){{ $message }}@enderror</span>
                     </div>
@@ -61,7 +63,7 @@
                 <div class="col-md-3">
                     <div class="form-group">
                         <label for="">Mother Name</label>
-                        <input type="text" class="form-control filter" placeholder="Enter your mother name"
+                        <input type="text" class="form-control form-text-input filter" placeholder="Enter your mother name"
                             name="mother_name">
                         <span class="text-danger">@error('mother_name'){{ $message }}@enderror</span>
                     </div>
@@ -69,55 +71,55 @@
                 <div class="col-md-3">
                     <div class="form-group">
                         <label for="">Father Name</label>
-                        <input type="text" class="form-control filter" placeholder="Enter your father name"
+                        <input type="text" class="form-control form-text-input filter" placeholder="Enter your father name"
                             name="father_name">
                         <span class="text-danger">@error('father_name'){{ $message }}@enderror</span>
                     </div>
                 </div>
-            </div>
+            </div> -->
             {{-- Physicall --}}
-            <div class="row">
+            <!-- <div class="row">
                 <div class="col-md-3">
                     <div class="form-group">
                         <label for="">Weight</label>
-                        <input type="text" class="form-control filter" placeholder="Enter weight" name="weight">
+                        <input type="text" class="form-control form-text-input filter" placeholder="Enter weight" name="weight">
                         <span class="text-danger">@error('weight'){{ $message }}@enderror</span>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="form-group">
                         <label for="">Height</label>
-                        <input type="text" class="form-control filter" placeholder="Enter height" name="height">
+                        <input type="text" class="form-control form-text-input filter" placeholder="Enter height" name="height">
                         <span class="text-danger">@error('height'){{ $message }}@enderror</span>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="form-group">
                         <label for="">Blood group</label>
-                        <select class="form-control filter" wire:model.live="blood_group">
-                            <option value="" selected>Choose blood group</option>
+                        <select class="form-control form-text-input filter" wire:model.live="blood_group">
+                            <option value="" selected >Choose blood group</option>
                             @foreach ($bloodGroups as $key => $title)
                             <option value="{{ $key }}">{{ $title }}</option>
                             @endforeach
                         </select>
                         <span class="text-danger">@error('blood_group'){{ $message }}@enderror</span>
                     </div>
-                </div>
-                <div class="col-md-3">
+                </div> -->
+                <!-- <div class="col-md-3">
                     <div class="form-group">
                         <label for="">Treating doctor</label>
-                        <input type="text" class="form-control filter" name="treating_doctor">
+                        <input type="text" class="form-control form-text-input filter" name="treating_doctor">
                         </select>
                         <span class="text-danger">@error('treating_doctor'){{ $message }}@enderror</span>
                     </div>
                 </div>
-            </div>
+            </div> -->
             {{-- Reffer, Campaign --}}
             <div class="row">
                 <div class="col-md-3">
                     <div class="form-group">
                         <label for="">Campaign channels</label>
-                        <select class="form-control filter" wire:model.live="compaignChannelId">
+                        <select class="form-control form-text-input filter" wire:model.live="compaignChannelId">
                             <option value="" selected>Choose channels</option>
                             @foreach ($campaignChannels as $channel)
                             <option value="{{ $channel->id }}">{{ $channel->title }}</option>
@@ -129,7 +131,7 @@
                 <div class="col-md-3">
                     <div class="form-group">
                         <label for="">Campaign organization</label>
-                        <select class="form-control filter" wire:model.live="campaign_org_id">
+                        <select class="form-control form-text-input filter" wire:model.live="campaign_org_id">
                             <option value="" selected>Choose organization</option>
                             @foreach ($campaignOrganizations as $org)
                             <option value="{{ $org->id }}">{{ $org->title }}</option>
@@ -141,7 +143,7 @@
                 <div class="col-md-3">
                     <div class="form-group">
                         <label for="">Reffering type</label>
-                        <select class="form-control filter" wire:model.live="reffering_type">
+                        <select class="form-control form-text-input filter" wire:model.live="reffering_type">
                             <option value="" selected>Choose reffering type</option>
                             @foreach ($refferingTypes as $key => $title)
                             <option value="{{ $key }}">{{ $title }}</option>
@@ -157,15 +159,15 @@
                 <div class="col-md-3">
                     <div class="form-group">
                         <label for="">Citizenship</label>
-                        <input type="text" class="form-control filter" placeholder="Enter citizenship"
+                        <input type="text" class="form-control form-text-input filter" placeholder="Enter citizenship"
                             name="citizenship">
                         <span class="text-danger">@error('citizenship'){{ $message }}@enderror</span>
                     </div>
                 </div>
-                <div class="col-md-3">
+                <!-- <div class="col-md-3">
                     <div class="form-group">
                         <label for="">Passport No</label>
-                        <input type="text" class="form-control filter" placeholder="Enter your passport no"
+                        <input type="text" class="form-control form-text-input filter" placeholder="Enter your passport no"
                             name="passport_no">
                         <span class="text-danger">@error('passport_no'){{ $message }}@enderror</span>
                     </div>
@@ -173,7 +175,7 @@
                 <div class="col-md-2">
                     <div class="form-group">
                         <label for="">Passport Origin</label>
-                        <input type="text" class="form-control filter" placeholder="Enter your passport origin"
+                        <input type="text" class="form-control form-text-input filter" placeholder="Enter your passport origin"
                             name="passport_origin">
                         <span class="text-danger">@error('passport_origin'){{ $message }}@enderror</span>
                     </div>
@@ -181,40 +183,40 @@
                 <div class="col-md-4">
                     <div class="form-group">
                         <label for="">Passport Image</label>
-                        <input type="file" class="form-control filter" placeholder="Enter your birth place"
+                        <input type="file" class="form-control form-text-input filter" placeholder="Enter your birth place"
                             name="passport_image">
                         <span class="text-danger">@error('passport_image'){{ $message }}@enderror</span>
                     </div>
-                </div>
+                </div> -->
             </div>
             {{-- Address --}}
             <div class="row">
                 <div class="col-md-3">
                     <div class="form-group">
                         <label for="">Email Address</label>
-                        <input type="email" class="form-control filter" placeholder="Enter email address" name="email">
+                        <input type="email" class="form-control form-text-input filter" placeholder="Enter email address" name="email">
                         <span class="text-danger">@error('email'){{ $message }}@enderror</span>
                     </div>
                 </div>
                 <div class="col-md-2">
                     <div class="form-group">
                         <label for="">Phone</label>
-                        <input type="text" class="form-control filter" placeholder="Enter your phone" name="phone">
+                        <input type="text" class="form-control form-text-input filter" placeholder="Enter your phone" name="phone">
                         <span class="text-danger">@error('phone'){{ $message }}@enderror</span>
                     </div>
                 </div>
-                <div class="col-md-2">
+                <!-- <div class="col-md-2">
                     <div class="form-group">
                         <label for="">Foriegn phone</label>
-                        <input type="text" class="form-control filter" placeholder="Enter your foriegn phone"
+                        <input type="text" class="form-control form-text-input filter" placeholder="Enter your foriegn phone"
                             name="foriegn_phone">
                         <span class="text-danger">@error('foriegn_phone'){{ $message }}@enderror</span>
                     </div>
-                </div>
+                </div> -->
                 <div class="col-md-2">
                     <div class="form-group">
                         <label for="">Countery</label>
-                        <select class="form-control filter" wire:model.live="countryId">
+                        <select class="form-control form-text-input filter" wire:model.live="countryId">
                             <option value=null>Select a country</option>
                             @foreach ($countries as $country)
                             <option value="{{ $country->id }}">{{ $country->name }}</option>
@@ -226,7 +228,7 @@
                 <div class="col-md-3">
                     <div class="form-group">
                         <label for="">City</label>
-                        <select class="form-control filter" wire:model.live="city_id">
+                        <select class="form-control form-text-input filter" wire:model.live="city_id">
                             <option>Select city</option>
                             @foreach ($cities as $city)
                             <option value="{{ $city->id }}">{{ $city->name }}</option>
@@ -265,3 +267,4 @@
         </form>
     </div>
 </div>
+</container>
