e29f291d7fb597280b4a870216b0480292ec86d8
Ahmet İzgördü <ahmet.izgordu@econtech.com.tr>
2024-07-01 10:33:35 +0300

    Fix

--- resources/views/admin/travels/relationships/formFilter.blade.php
+++ resources/views/admin/travels/relationships/formFilter.blade.php
@@ -11,32 +11,32 @@ Vaka Listesi
 
             <div class="row">
                 <div class="col-md-4">
-                    <div class="form-group">
-                    <label for="">Hasta Kodu</label>
-                        <input type="text" class="form-control filter" placeholder="Hasta Kodu"
-                            name="patient_code">
+                    <div class="floating-label">
+                        <input type="text" class="floating-input filter" 
+                            name="patient_code" placeholder=" ">
+                            <label >Hasta Kodu</label>
                         <span class="text-danger">@error('patient_code'){{ $message }}@enderror</span>
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <div class="form-group">
-                    <label for="">Hasta Adı</label>
-                        <input type="text" class="form-control filter" placeholder="Hasta Adı"
+                    <div class="floating-label">
+                        <input type="text" class="floating-input filter" placeholder=""
                             name="patient_name">
-
+                            <label for="">Hasta Adı</label>
                         <span class="text-danger">@error('patient_name'){{ $message }}@enderror</span>
                     </div>
                 </div>
                 
                 <div class="col-md-4">
-                    <div class="form-group">
-                    <label for="">Cinsiyet</label>
-                        <select class="form-control filter" wire:model.live="gender">
-                            <option value="" selected>Cinsiyet</option>
+                    <div class="floating-label">
+                        <select class="floating-select filter" wire:model.live="gender" onclick="this.setAttribute('value', this.value);" value="">
+                            <option value="" > </option>
                             @foreach ($genders as $key => $title)
                             <option value="{{ $key }}">{{ $title }}</option>
                             @endforeach
                         </select>
+                        <label for="">Cinsiyet</label>
+
                         <span class="text-danger">@error('gender'){{ $message }}@enderror</span>
                     </div>
                 </div>
@@ -202,3 +202,46 @@ Vaka Listesi
     </div>
 </div>
 </container>
+                            <!--Test input verileri edit sayfası bitene kadar örnek olması için
+                   
+<div class="floating-label">      
+      <input class="floating-input" type="text" placeholder=" ">
+      <label>Text</label>
+    </div>
+
+
+    <div class="floating-label">      
+      <input class="floating-input" type="text" onclick="(this.type='date')" placeholder=" ">
+      <span class="highlight"></span>
+      <label>Date</label>
+    </div>
+    <div class="floating-label">      
+      <input class="floating-input" type="password" placeholder=" ">
+      <span class="highlight"></span>
+      <label>Password</label>
+    </div>
+    <div class="floating-label">  
+      <select class="floating-select" onclick="this.setAttribute('value', this.value);" value="">
+        <option value="" disabled selected></option>
+        <option value="1">Alabama</option>
+        <option value="2">Boston</option>
+        <option value="3">Ohaio</option>
+        <option value="4">New York</option>
+        <option value="5">Washington</option>
+      </select>
+      <span class="highlight"></span>
+      <label>Select</label>
+    </div>
+
+    <div class="floating-label">      
+      <textarea class="floating-input floating-textarea" placeholder=" "></textarea>
+      <span class="highlight"></span>
+      <label>Textarea</label>
+    </div>
+
+
+                            
+                            
+                            
+                            
+                            -->
