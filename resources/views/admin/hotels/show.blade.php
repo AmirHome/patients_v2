7f6f85b4feaad1c9dcdbaaa2b605289223487645
Ahmet İzgördü <ahmet.izgordu@econtech.com.tr>
2024-08-23 15:33:01 +0300

    fix: hotel show

--- resources/views/admin/hotels/show.blade.php
+++ resources/views/admin/hotels/show.blade.php
@@ -5,21 +5,18 @@
 <div class="container">
     <div class="hotel-header">
         <h1 class="hotel-name">{{ $hotel->name }}</h1>
-        <p class="hotel-location">{{ $hotel->location }}, {{ $hotel->city->name ?? '' }}, {{ $hotel->country->name ?? '' }}</p>
+        <p class="hotel-location"> {{ $hotel->city->name ?? '' }}, {{ $hotel->country->name ?? '' }}</p>
     </div>
 
     <div class="hotel-images">
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
-                @foreach ($imgs??[]  as $id => $img)
-                    <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
-                        <img class="d-block w-100" src="{{ asset('path/to/images/'.$img) }}" alt="Image {{ $id }}">
-                    </div>
+               @foreach($hotel->photos as $key => $media)
+                    <a href="{{ $media->getUrl() }}" target="_blank" style="display: inline-block">
+                        <img src="{{ asset('img/img-not-found.png')  }}">
+                    </a>
                 @endforeach
             </div>
-
-
-
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
@@ -32,81 +29,17 @@
     </div>
 
     <div class="hotel-details">
-        <h2>  {{ trans('cruds.hotel.fields.location') }}</h2>
+        <h2>  {{ trans('cruds.hotel.fields.location') }} {{ $hotel->location }},</h2>
         <p><strong>{{ trans('cruds.hotel.fields.price') }}:</strong> {{ $hotel->price }}</p>
     </div>
 
-    <div class="back-to-list">
+    <div class="back-to-list mt-5">
         <a class="btn btn-primary" href="{{ route('admin.hotels.index') }}">
             {{ trans('global.back_to_list') }}
         </a>
     </div>
 </div>
 
-<table class="table table-bordered table-striped">
-    <tbody>
-        <tr>
-            <th>
-                {{ trans('cruds.hotel.fields.id') }}
-            </th>
-            <td>
-                {{ $hotel->id }}
-            </td>
-        </tr>
-        <tr>
-            <th>
-                {{ trans('cruds.hotel.fields.name') }}
-            </th>
-            <td>
-                {{ $hotel->name }}
-            </td>
-        </tr>
-        <tr>
-            <th>
-                {{ trans('cruds.hotel.fields.location') }}
-            </th>
-            <td>
-                {{ $hotel->location }}
-            </td>
-        </tr>
-        <tr>
-            <th>
-                {{ trans('cruds.hotel.fields.price') }}
-            </th>
-            <td>
-                {{ $hotel->price }}
-            </td>
-        </tr>
-        <tr>
-            <th>
-                {{ trans('cruds.hotel.fields.country') }}
-            </th>
-            <td>
-                {{ $hotel->country->name ?? '' }}
-            </td>
-        </tr>
-        <tr>
-            <th>
-                {{ trans('cruds.hotel.fields.city') }}
-            </th>
-            <td>
-                {{ $hotel->city->name ?? '' }}
-            </td>
-        </tr>
-        <tr>
-            <th>
-                {{ trans('cruds.hotel.fields.photos') }}
-            </th>
-            <td>
-                @foreach($hotel->photos as $key => $media)
-                    <a href="{{ $media->getUrl() }}" target="_blank" style="display: inline-block">
-                        <img src="{{ $media->getUrl() ?? 'https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/no-profile-picture-icon.png' }}">
-                    </a>
-                @endforeach
-            </td>
-        </tr>
-    </tbody>
-</table>
 
 
 <style>
@@ -142,7 +75,12 @@
 
     .back-to-list {
         text-align: center;
-        margin-top: 30px;
+    }
+    .carousel-inner img {
+        max-height: 450px !important;
+        min-height: 250px !important;
+        background-size: cover;
+        width: 100%;
     }
 </style>
 @endsection
